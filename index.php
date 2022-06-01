6e37ae458fb3373a77c24bfb6ae45288406e5281
ketjehdev <ketjeh.developer@gmail.com>
2022-06-02 04:32:37 +0700

    update modal register

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/app.css">
+	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/splash.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -442,19 +442,16 @@
 	<div class="modal fade" id="register" aria-hidden="true" aria-labelledby="registerLabel" tabindex="-1">
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="registerLabel">Registrasi Member</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
 				<div class="modal-body d-flex flex-column">
 					<p class="text-center">Daftarkan dirimu sebagai member sekarang!</p>
 					<img src="asset/svg/register1.svg" alt="register member" width="100%">
 					<a href="register.php">
-						<button class="btn btn-success text-light" style="width: 100%;">Daftar sebagai member</button>
+						<button class="btn btn-success text-light mb-2" style="width: 100%;">Daftar sebagai member</button>
 					</a>
-				</div>
-				<div class="modal-footer">
-					<button class="btn btn-secondary" data-bs-target="#register2" data-bs-toggle="modal" data-bs-dismiss="modal">Lainnya &RightArrow;</button>
+					<div class="d-flex">
+						<button type="button" class="btn btn-danger" data-bs-dismiss="modal" style="width: 50%; margin-right: 5px;">Exit</button>
+						<button class="btn btn-secondary" style="width: 50%;" data-bs-target="#register2" data-bs-toggle="modal" data-bs-dismiss="modal">Lainnya</button>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -464,19 +461,16 @@
 	<div class="modal fade" id="register2" aria-hidden="true" aria-labelledby="registerLabel2" tabindex="-1">
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="registerLabel2">Registrasi Pengepul</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
 				<div class="modal-body d-flex flex-column">
 					<p class="text-center">Daftarkan dirimu sebagai pengepul sekarang!</p>
 					<img src="asset/svg/register2.svg" alt="register pengepul" width="100%">
 					<a href="register_pengepul.php">
-						<button class="btn btn-primary text-light" style="width: 100%;">Daftar sebagai pengepul</button>
+						<button class="btn btn-primary text-light mb-2" style="width: 100%;">Daftar sebagai pengepul</button>
 					</a>
-				</div>
-				<div class="modal-footer">
-					<button class="btn btn-secondary" data-bs-target="#register" data-bs-toggle="modal" data-bs-dismiss="modal">&LeftArrow; Kembali</button>
+					<div class="d-flex">
+						<button class="btn btn-secondary" style="width: 50%;" data-bs-target="#register" data-bs-toggle="modal" data-bs-dismiss="modal">Kembali</button>
+						<button type="button" class="btn btn-danger" data-bs-dismiss="modal" style="width: 50%; margin-left: 5px;">Exit</button>
+					</div>
 				</div>
 			</div>
 		</div>
